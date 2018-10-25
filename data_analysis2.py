154dfd82f67bb9f56dd78e37320437fe275dae9f
luwenlong123 <2318804431@qq.com>
2018-10-25 16:41:05 +0800

    Add files via upload

--- data_analysis2.py
+++ data_analysis2.py
@@ -20,3 +20,11 @@ data.years[8896:47945] = 2017
 data.years = data.years.astype('str')
 data['date'] = data['years'] + data['fz'] + data['pubtime']
 data2 = data.iloc[0:10606,:]
+data3 = data2.iloc[:,[0,1,2,3,10]]
+
+s = data3.groupby(by='date')
+df1 = s.count()
+df2 = s.sum()
+df2.columns=['read_sum','dis_sum']
+all_df = hq_df.join(df1)
+all_df = all_df.join(df2)
